033f88ab18657b0477c128efb9f53d03cf45d995
gustmacena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 21:53:45 -0300

    Maior Menor e Igual

---
+++ maior_menor_igual.js
@@ -0,0 +1,15 @@
+let x = 30
+let y = 31
+
+if(x > y){
+
+    console.log("Maior é o numero: " + x)
+
+}else if(x < y){
+
+    console.log("Maior é o numero: " + y)
+
+} else{
+
+    console.log("Os numeros sao iguais!")
+}
